9d9cd6f26489b8cbe222d22fe484c5a15fadbeb8
llZektorll <89364200+llZektorll@users.noreply.github.com>
2022-01-24 13:37:56 +0000

    Add files via upload

---
+++ DDC/DDC_Folder_Maker.ps1
@@ -0,0 +1,12 @@
+﻿$folder="X:\Test";             # Directory to place the new folders in.
+$txtFile="X:\Test\Export.txt"; # File with list of new folder-names
+$pattern="\d+.+";              # Pattern that lines must match      
+
+
+get-content $txtFile | %{
+
+    if($_ -match $pattern)
+    {
+        mkdir "$folder\$_";
+    }
+}
